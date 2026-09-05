bafc55c06ea1c7dd49027bcf315dc8fddbbe4d7f
vasavi <subbu.p121980@gmail.com>
2026-09-05 16:01:44 +0530

    Fix production CORS origin

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://fast-api-project-mu-gules.vercel.app/"],
+    allow_origins=["http://localhost:5173","https://fast-api-project-mu-gules.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
